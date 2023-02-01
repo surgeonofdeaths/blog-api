61bb0389458ef9802aeff64b29e36721bd4b1223
Artem Holodyr <artemmholodyr@gmail.com>
2023-02-01 18:28:30 +0200

    add permission to user viewset

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,7 @@ from .views import PostViewSet, UserViewSet
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
-router.register(prefix='', viewset=PostViewSet, basename='posts')
 router.register('users', UserViewSet, basename='users')
+router.register(prefix='', viewset=PostViewSet, basename='posts')
+
 urlpatterns = router.urls
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
-# from rest_framework import generics
 from rest_framework import viewsets
 from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAdminUser
 
 from .serializers import PostSerializer, UserSerializer
 from .models import Post
@@ -16,4 +16,5 @@ class PostViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = get_user_model().objects.all()
+    permission_classes = (IsAdminUser, )
 
